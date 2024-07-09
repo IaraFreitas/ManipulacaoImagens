ede193ddd7e44c13d9e88e79566a1088ab971418
@if.dev_ <iara.freitas.job@gmail.com>
2024-07-08 21:58:29 -0300

    update

---
+++ Script.js
@@ -0,0 +1,37 @@
+function mudarBarbeador(imagem) {
+    imagem.src = 'img/barbeador-antigo.jpg';
+};
+
+function mudarOutraBarbeador(imagem) {
+    imagem.src = 'img/barbeador-moderno.jpg';
+};
+
+
+
+function mudarBoliche(imagem) {
+    imagem.src = 'img/boliche-antigo.jpg';
+};
+
+function mudarOutraBoliche(imagem) {
+    imagem.src = 'img/boliche-moderno.jpg';
+};
+
+
+
+function mudarBolo(imagem) {
+    imagem.src = 'img/bolo-antigo.jpg';
+};
+
+function mudarOutraBolo(imagem) {
+    imagem.src = 'img/bolo-moderno.jpg';
+};
+
+
+
+function mudarCrianca(imagem) {
+    imagem.src = 'img/crianças-antigamente.jpg';
+};
+
+function mudarOutraCrianca(imagem) {
+    imagem.src = 'img/criança-moderna.jpg';
+};
